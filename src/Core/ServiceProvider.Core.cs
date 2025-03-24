69ee7967a1b0325f2ab3503bcd2348e408f0a8a0
poproshaikin <stasstrong06@gmail.com>
2025-03-24 21:24:36 +0100

    split facade class into two parts: with core logic and generic methods overloads

--- src/Core/ServiceProvider.Core.cs
+++ src/Core/ServiceProvider.Core.cs
@@ -1,14 +1,38 @@
 namespace Power.DI;
 
-
-public class ServiceProvider : IPowerServiceProvider 
-{
-    public void Add<TService, TImplementation>(string? key = null) where TService : class where TImplementation : class, TService
+public partial class ServiceProvider : IPowerServiceProvider 
+{    
+    public void AddSingleton(Type serviceType, Type implType, string? key = null)
     {
         throw new NotImplementedException();
     }
 
-    public void AddFactory<TService, TImpl>(Func<TImpl> factory) where TImpl : class, TService
+    public void AddSingleton(Type implType, string? key = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddTransient(Type serviceType, Type implType, string? key = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddTransient(Type implType, string? key = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddScoped(Type serviceType, Type implType, string? key = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddScoped(Type implType, string? key = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddFactory(Type serviceType, Type implType, Func<object> factory)
     {
         throw new NotImplementedException();
     }
@@ -18,12 +42,7 @@ public class ServiceProvider : IPowerServiceProvider
         throw new NotImplementedException();
     }
 
-    public TService Resolve<TService>(string? key = null) where TService : class
-    {
-        throw new NotImplementedException();
-    }
-
-    public IEnumerable<TService> ResolveAll<TService>() where TService : class
+    public IEnumerable<object> ResolveAll(Type serviceType)
     {
         throw new NotImplementedException();
     }
