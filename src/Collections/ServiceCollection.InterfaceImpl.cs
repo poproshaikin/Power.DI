f89bfbd2e1a9d53e317e94b3a9e56479b8b52de7
poproshaikin <stasstrong06@gmail.com>
2025-03-24 20:23:56 +0100

    remove old files

--- src/Collections/ServiceCollection.InterfaceImpl.cs
+++
@@ -1,9 +0,0 @@
-namespace Power.DI;
-
-public partial class ServiceCollection
-{
-    void IServiceCollection.Add<TService, TImplementation>(string? key) =>
-        Add<TService, TImplementation>(key);
-
-    void IServiceCollection.Add<TImplementation>(string? key) => Add<TImplementation>(key);
-}
--- src/Collections/TransientServicesCollection.cs
+++
@@ -1,9 +0,0 @@
-namespace Power.DI;
-
-internal class TransientServicesCollection : IServiceCollection
-{
-    public void Add<TService, TImplementation>(string? key = null) where TService : class where TImplementation : class, TService
-    {
-        throw new NotImplementedException();
-    }
-}
